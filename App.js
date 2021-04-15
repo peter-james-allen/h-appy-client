deb04b58c48f8d81a88f38366077cb6511cdd0cd
Adam Jonas <adamjonas25@gmail.com>
2021-04-15 19:31:14 +0100

    Add safe area view wrapper to app

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-use-before-define */
 import React, { useEffect, useState } from 'react';
 import {
-  StyleSheet, Text, View, Button, Alert, FlatList, Image, ActivityIndicator,
+  StyleSheet, Text, View, Button, Alert, FlatList, Image, ActivityIndicator, SafeAreaView,
 } from 'react-native';
 import { DrawerNavigator } from './routes/DrawerNavigator';
 
 export default function App() {
   return (
-    <DrawerNavigator />
+    <SafeAreaView style={styles.contentWrapper}>
+      <DrawerNavigator />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  contentWrapper: {
+    flex: 1,
+    backgroundColor: '#f8f9d4'
+  }
+})
