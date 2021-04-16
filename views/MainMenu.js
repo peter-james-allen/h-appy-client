c25375d4abd330aabd34ac2ba7a3d0d45c192de0
Jack Gumoes <j.gumoes@gmail.com>
2021-04-16 17:10:29 +0100

    MainMenu now sets userData through useState

--- views/MainMenu.js
+++ views/MainMenu.js
@@ -7,8 +7,11 @@ import {
 import { useNavigation } from '@react-navigation/native';
 import fetch from 'node-fetch';
 import Header from '../components/Header';
+import hardUserData from '../components/UserData';
 
 export default function MainMenu() {
+  const [userData, setUserData] = useState(hardUserData);
+  
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
@@ -157,52 +160,6 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
 });
-
-const userData = {
-  nibbles: [
-    {
-      _id: 'bd7dcbea-c1b1-46c2-aed5-3ad53abb28ba',
-      name: 'Go to the Cinema',
-      ingredients: [],
-    },
-    {
-      _id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      name: 'Do a Puzzle',
-      ingredients: ['a phone or computer or puzzle book'],
-    },
-    {
-      _id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      name: 'Take a long awaited break',
-      ingredients: [],
-    },
-  ],
-  appetisers: [
-    {
-      _id: 'bd7acbea-c1b1-46c2-aed5-3ad53dbb28ba',
-      name: 'Do a codewars kata',
-      ingredients: ['computer'],
-    },
-    {
-      _id: 'bd0acjea-c4b1-46c2-red5-3ad53abb28ba',
-      name: 'Play a piece of music',
-      ingredients: ['an instrument', 'somewhere private'],
-    },
-  ],
-  mains: [
-    {
-      _id: 'ai589cm1-oi5n-alf3-bd96-145571e29d72',
-      name: 'Learn a new song on the guitar',
-      ingredients: ['a guitar'],
-    },
-  ],
-  desserts: [
-    {
-      _id: '3ac68afc-dk30-3kf9-a4f8-fbd91aa9d07k',
-      name: 'Browse Reddit for 3 hours',
-      ingredients: ['a phone', 'Ennui'],
-    },
-  ],
-};
 
 const badNetworkApiData = [
   {
