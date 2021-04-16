8cb95649357e794f881e20d5f802f61fd41143fc
Jack Gumoes <j.gumoes@gmail.com>
2021-04-16 16:29:41 +0100

    starting creating AddActivity form

--- views/AddActivity.js
+++ views/AddActivity.js
@@ -1,14 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
-  StyleSheet, Text, View, Button, Alert, FlatList, ActivityIndicator,
+  Text, View, TextInput, StyleSheet,
 } from 'react-native';
 import Header from '../components/Header';
 
 export default function AddActivity() {
+  const [text, setText] = useState('');
   return (
-    <View>
+    <View style={styles.container}>
+      <Header />
       <Text> This will be we can add activities as a user </Text>
+
+      <View style={styles.FormItem}>
+        <Text>Activity Type</Text>
+      </View>
+
+      <View style={styles.FormItem}>
+        <Text>Activity Name</Text>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="enter the name of the activity here"
+          onChangeText={(text) => setText(text)}
+          defaultValue={text}
+        />
+      </View>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f8f9d4',
+    alignContent: 'flex-start',
+  },
+  FormItem: {
+    margin: 3,
+    borderWidth: 2,
+    width: '80%',
+  },
+});
