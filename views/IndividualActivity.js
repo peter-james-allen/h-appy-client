9b99fbc6ab078789d0d69fc802cbcfbff387de7a
Adam Jonas <adamjonas25@gmail.com>
2021-04-19 16:23:26 +0100

    Add headers to grid of activity details

--- views/IndividualActivity.js
+++ views/IndividualActivity.js
@@ -14,27 +14,58 @@ import react from 'react';
 
 const numColumns = 2;
 const size = Dimensions.get('window').width / numColumns - 40;
+const height = size / 1.3;
 
 function Grid(props) {
   const { activity } = props;
+  const categories = activity.categories.join('\n');
   const data = [
+    { id: 'header0', value: 'Size' },
+    { id: 'header1', value: 'Categories' },
     { id: 'size', value: activity.size },
-    { id: 'categories', value: activity.categories },
-    { id: 'cost', value: `${activity.cost}/10\ncost` },
-    { id: 'accessibility', value: `${activity.accessibility}/10\naccessibility` },
+    { id: 'categories', value: categories },
+    { id: 'header2', value: 'Cost' },
+    { id: 'header3', value: 'Acccessibility' },
+    { id: 'cost', value: `${activity.cost}/10` },
+    { id: 'accessibility', value: `${activity.accessibility}/10` },
   ];
 
   return (
     <FlatList
       data={data}
       renderItem={({item}) => (
-        <View style={styles.itemContainer}>
-          <Text style={styles.item}>{item.value}</Text>
+        <View style={containerStyle(item)}>
+          <Text style={[styles.item, itemStyle(item)]}>{item.value}</Text>
         </View>
       )}
       keyExtractor={item => item.id}
       numColumns={numColumns} />
   );
+}
+
+function containerStyle(item) {
+  const itemContainer = {
+    width: size,
+    height: height,
+  }
+
+  if (item.id.includes('header')) {
+    return {
+      width: size,
+      height: size / 4,
+    }
+  }
+  return itemContainer
+}
+
+function itemStyle(item) {
+  const padding = {
+    lineHeight: height
+  }
+
+  if (!item.id.includes('header')) {
+    return padding
+  }
 }
 
 export default function IndividualActivity({ route }) {
@@ -59,24 +90,6 @@ export default function IndividualActivity({ route }) {
         <Grid activity={route.params.item} />
       </View>
 
-      {/* <View style={styles.detailsFlex}>
-        <Text style={styles.individualDetail}>
-          {route.params.item.size}
-        </Text>
-
-        <Text style={styles.individualDetail}>
-         {route.params.item.accessibility}/10 accessibility
-        </Text>
-
-        <Text style={styles.individualDetail}>
-          {route.params.item.cost}/10 cost
-        </Text>
-
-        <Text style={styles.individualDetail}>
-          {route.params.item.categories}
-        </Text>
-      </View> */}
-
       {/* <Button
         style={styles.menuButton}
         title="Back to the Main Menu"
@@ -99,7 +112,7 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 20,
     lineHeight: 25,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   descriptionContainer: {
     width: '80%',
@@ -146,14 +159,14 @@ const styles = StyleSheet.create({
   itemContainer: {
     width: size,
     height: size,
-
   },
   item: {
     flex: 1,
-    margin: 3,
-    backgroundColor: 'lightblue',
+    margin: 1,
+    fontSize: 25,
+    backgroundColor: '#cc3d3d',
     textAlign: 'center',
-    paddingTop: '40%'
+    color: '#fff',
   },
   grid: {
     position: 'absolute',
