ac81bf96890815c80a944199c81afb2d4db8d29a
iainaitken <iain_aitken@yahoo.co.uk>
2021-04-20 14:48:14 +0100

    Part way through adding user authentication

--- App.js
+++ App.js
@@ -6,9 +6,69 @@ import {
 import AppNavigator from './routes/AppNavigator';
 import DrawerNavigator from './routes/DrawerNavigator';
 import FlashMessage from 'react-native-flash-message';
+import SecureStore from 'expo-secure-store';
+import sendAuthenticationData from './src/AuthenticationData';
 
+const AuthContext = React.createContext();
 
 export default function App() {
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  React.useEffect(() => {
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await SecureStore.getItemAsync('userToken');
+      } catch (error) {
+
+      }
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: sendAuthenticationData(emailData, passwordData, navigation);
+      dispatch({ type: 'SIGN_IN', token: })
+    })
+  )
+  
+  
+  
+  
+  
+  
   return (
     <>
       <SafeAreaView style={styles.safeAreaTop} />
