0e50b9a26ea26913b0478bd0211a97896ebd9e44
Adam Jonas <adamjonas25@gmail.com>
2021-04-21 19:41:37 +0100

    Change main menu title to chalkboard

--- components/MenuTitle.js
+++ components/MenuTitle.js
@@ -1,13 +1,15 @@
 import {
-  View, Text, StyleSheet, Platform,
+  View, Text, StyleSheet, Platform, Dimensions,
 } from 'react-native';
 import React from 'react';
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function MenuTitle() {
   return (
     <View style={styles.titleContainer}>
       <Text style={styles.menuTitle}>
-        Feed your Boredom
+        Feed your boredom...
       </Text>
     </View>
   );
@@ -15,19 +17,36 @@ export default function MenuTitle() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flex: 0.1,
-    marginTop: 45,
-    marginBottom: 15,
+    flex: 0.15,
+    width: windowWidth * 0.939,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#819595',
-    width: '95%',
-    padding: 25,
+    backgroundColor: '#363946',
     borderRadius: 7,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+    marginBottom: '5%',
+    marginTop: '15%'
   },
   menuTitle: {
-    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Didot',
-    color: '#363946',
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Chalkduster',
+    color: '#fff',
     fontSize: 20,
   },
 });
+
+// flex: 0.1,
+// marginTop: 45,
+// marginBottom: 15,
+// alignItems: 'center',
+// justifyContent: 'center',
+// backgroundColor: '#819595',
+// width: '95%',
+// padding: 25,
+// borderRadius: 7,
