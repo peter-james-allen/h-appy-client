25cb83dfeabcd4d104c800acc00f77e033b7bf0f
Peter Allen <peter_james_allen@hotmail.com>
2021-04-22 13:41:45 +0100

    updated about page styling

--- views/About.js
+++ views/About.js
@@ -1,14 +1,19 @@
-import React, { Component, useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React, { Component, useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 import {
-  StyleSheet, Text, View, TouchableOpacity, Image, Dimensions,
-} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import Header from '../components/Header';
-import Anchor from '../components/Anchor';
-import MenuButton from '../components/MenuButton';
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Header from "../components/Header";
+import Anchor from "../components/Anchor";
+import MenuButton from "../components/MenuButton";
 
-const windowWidth = Dimensions.get('window').width;
+const windowWidth = Dimensions.get("window").width;
 
 export default function About() {
   const navigation = useNavigation();
@@ -16,31 +21,38 @@ export default function About() {
     <View style={styles.container}>
       <Header />
       <View style={styles.aboutFlex}>
-        <Image style={styles.logo} source={require('../assets/logoH.png')} />
+        <Image style={styles.logo} source={require("../assets/logoH2.png")} />
         <Text style={styles.about}>
-          {'\n'}
+          {"\n"}
           About H-Appy
         </Text>
       </View>
       <View style={styles.aboutContainer}>
         <Text style={styles.blurb}>
-          When you're bored, it can be difficult to choose an activity to relieve that boredom, in the same way that it can be difficult to make good food decisions when you're already hungry. Often, we end up doing things that just don't make us feel any better - like spending all day on social media, or eating unhealthy food.
-          {'\n'}
-          {'\n'}
-          A dopamine menu is a list of activities grouped into different categories. It can help you decide what to do to alleviate your boredom in an appropriate way.
-
-          Press the button below to learn more about the dopamine menu.
-          {'\n'}
+          When you're bored, it can be difficult to choose an activity to
+          relieve that boredom, in the same way that it can be difficult to make
+          good food decisions when you're already hungry. Often, we end up doing
+          things that just don't make us feel any better - like spending all day
+          on social media, or eating unhealthy food.
+          {"\n"}
+          {"\n"}A dopamine menu is a list of activities grouped into different
+          categories. It can help you decide what to do to alleviate your
+          boredom in an appropriate way.{"\n"}
+          {"\n"}Press the button below to learn more about the dopamine menu.
+          {"\n"}
         </Text>
         <Anchor
-          style={{ alignItems: 'center', margin: 20 }}
+          style={{ alignItems: "center", margin: 20 }}
           href="https://www.youtube.com/watch?v=-6WCkTwW6xg"
         >
-          <Ionicons style={{ alignContent: 'center', position: 'absolute' }} name="logo-youtube" size={30} color="#363946" />
+          <Ionicons
+            style={{ alignContent: "center", position: "absolute" }}
+            name="logo-youtube"
+            size={30}
+            color="#363946"
+          />
         </Anchor>
-        <Text>
-          {'\n'}
-        </Text>
+        <Text>{"\n"}</Text>
       </View>
       <MenuButton />
     </View>
@@ -50,21 +62,22 @@ export default function About() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#696773',
-    height: Dimensions.get('window').height,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#696773",
+    height: Dimensions.get("window").height,
     width: windowWidth,
   },
   aboutFlex: {
+    borderRadius: 5,
     margin: 20,
     padding: 20,
-    width: '50%',
+    width: windowWidth * 0.93,
     minHeight: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#363946',
-    shadowColor: '#000',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#363946",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 8,
@@ -75,27 +88,35 @@ const styles = StyleSheet.create({
   },
   about: {
     fontSize: 18,
-    textAlign: 'center',
-    maxWidth: '90%',
-    fontFamily: 'Chalkduster',
-    color: 'white',
+    textAlign: "center",
+    maxWidth: "90%",
+    fontFamily: "Chalkduster",
+    color: "white",
   },
   aboutContainer: {
-    width: windowWidth * 0.7,
-    backgroundColor: '#B1B6A6',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: windowWidth * 0.93,
+    backgroundColor: "#B1B6A6",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 5,
     padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
   },
   blurb: {
     fontSize: 14,
     lineHeight: 15,
-    textAlign: 'center',
+    textAlign: "center",
     padding: 10,
     top: 5,
-    color: '#23252E',
-    fontFamily: 'Courier',
+    color: "#23252E",
+    fontFamily: "Courier",
   },
   logo: {
     width: 200,
